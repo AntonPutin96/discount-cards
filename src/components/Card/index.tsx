5fa60940b5476aab0f2a47df510f9529bd6dbe0d
ighhe <igthr@rambler.ru>
2024-03-15 18:25:08 +0300

    button

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ButtonBase,
   Card as CardMui,
   CardActionArea,
   CardContent,
   CardMedia,
-  Typography
+  Typography,
+  CardActions
 } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShopImage from '../../images/shop_img_345_140.jpg';
 
 interface CardProps {
@@ -14,24 +17,39 @@ interface CardProps {
   onClick?: () => void;
 }
 
-const Card = ({ title, onClick }: CardProps) => (
-  <CardMui sx={{ maxWidth: 345 }}>
-    <ButtonBase onClick={onClick}>
+const Card = ({ title, onClick }: CardProps) => {
+  const [favorite, setFavorite] = useState(false);
+
+  const cardFavorite = () => {
+    setFavorite(!favorite);
+  };
+
+  return (
+    <CardMui sx={{ maxWidth: 345 }}>
       <CardActionArea>
-        <CardMedia
-          component='img'
-          height='140'
-          image={ShopImage}
-          alt='green iguana'
-        />
+        <ButtonBase onClick={onClick}>
+          <CardMedia
+            component='img'
+            height='140'
+            image={ShopImage}
+            alt='green iguana'
+          />
+        </ButtonBase>
         <CardContent>
-          <Typography gutterBottom variant='h5' component='div'>
-            {title}
-          </Typography>
+          <CardActions disableSpacing>
+            <IconButton
+              onClick={() => cardFavorite()}
+              color={`${favorite ? 'primary' : 'default'}`}
+              aria-label='add to favorites'
+            >
+              <FavoriteIcon />
+            </IconButton>
+            <Typography component='div'>{title}</Typography>
+          </CardActions>
         </CardContent>
       </CardActionArea>
-    </ButtonBase>
-  </CardMui>
-);
+    </CardMui>
+  );
+};
 
 export default Card;
